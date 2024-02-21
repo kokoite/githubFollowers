24045d988d60d92afffd99354d3de8f4fb4f7df0
Pranjal <kokoite908@gmail.com>
2024-02-21 20:57:55 +0530

    Basic setup

---
+++ Github-Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github-Followers
+//
+//  Created by Pranjal Agarwal on 21/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
